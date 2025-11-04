8e87eab16b542b081630b671ddb0d5f989cf78cf
Isa2322 <altamirandaisaias@gmail.com>
2025-11-03 21:30:23 -0300

    main + lo mio 3/11 21:30

--- 00_GeneradorObjetos.sql
+++ 00_GeneradorObjetos.sql
@@ -14,38 +14,38 @@ Pastori, Ximena - 42300128
 
 Enunciado:
 Base de datos lineamientos generales
-Se requiere que importe toda la información antes mencionada a la base de datos:
-• Genere los objetos necesarios (store procedures, funciones, etc.) para importar los
-archivos antes mencionados. Tenga en cuenta que cada mes se recibirán archivos de
+Se requiere que importe toda la informacion antes mencionada a la base de datos:
+- Genere los objetos necesarios (store procedures, funciones, etc.) para importar los
+archivos antes mencionados. Tenga en cuenta que cada mes se recibiran archivos de
 novedades con la misma estructura, pero datos nuevos para agregar a cada maestro.
-• Considere este comportamiento al generar el código. Debe admitir la importación de
-novedades periódicamente sin eliminar los datos ya cargados y sin generar
+- Considere este comportamiento al generar el codigo. Debe admitir la importacion de
+novedades periodicamente sin eliminar los datos ya cargados y sin generar
 duplicados.
-• Cada maestro debe importarse con un SP distinto. No se aceptarán scripts que
-realicen tareas por fuera de un SP. Se proveerán archivos para importar en MIEL.
-• La estructura/esquema de las tablas a generar será decisión suya. Puede que deba
-realizar procesos de transformación sobre los maestros recibidos para adaptarlos a la
-estructura requerida. Estas adaptaciones deberán hacerla en la DB y no en los
+- Cada maestro debe importarse con un SP distinto. No se aceptaran scripts que
+realicen tareas por fuera de un SP. Se proveeran archivos para importar en MIEL.
+- La estructura/esquema de las tablas a generar sera decision suya. Puede que deba
+realizar procesos de transformacion sobre los maestros recibidos para adaptarlos a la
+estructura requerida. Estas adaptaciones deberan hacerla en la DB y no en los
 archivos provistos.
-• Los archivos CSV/JSON no deben modificarse. En caso de que haya datos mal
-cargados, incompletos, erróneos, etc., deberá contemplarlo y realizar las correcciones
-en la fuente SQL. (Sería una excepción si el archivo está malformado y no es posible
+- Los archivos CSV/JSON no deben modificarse. En caso de que haya datos mal
+cargados, incompletos, erroneos, etc., debera contemplarlo y realizar las correcciones
+en la fuente SQL. (Seria una excepcion si el archivo esta malformado y no es posible
 interpretarlo como JSON o CSV, pero los hemos verificado cuidadosamente).
-• Tener en cuenta que para la ampliación del software no existen datos; se deben
+- Tener en cuenta que para la ampliacion del software no existen datos; se deben
 preparar los datos de prueba necesarios para cumplimentar los requisitos planteados.
-• El código fuente no debe incluir referencias hardcodeadas a nombres o ubicaciones
-de archivo. Esto debe permitirse ser provisto por parámetro en la invocación. En el
-código de ejemplo se verá dónde el grupo decidió ubicar los archivos, pero si cambia
-el entorno de ejecución debería adaptarse sin modificar el fuente (sí obviamente el
-script de testing). La configuración escogida debe aparecer en comentarios del
+- El codigo fuente no debe incluir referencias hardcodeadas a nombres o ubicaciones
+de archivo. Esto debe permitirse ser provisto por parametro en la invocacion. En el
+codigo de ejemplo se vera donde el grupo decidio ubicar los archivos, pero si cambia
+el entorno de ejecucion debería adaptarse sin modificar el fuente (si obviamente el
+script de testing). La configuracion escogida debe aparecer en comentarios del
 módulo.
-• El uso de SQL dinámico no está exigido en forma explícita… pero puede que
-encuentre que es la única forma de resolver algunos puntos. No abuse del SQL
-dinámico, deberá justificar su uso siempre.
-• Respecto a los informes XML: no se espera que produzcan un archivo nuevo en el
+- El uso de SQL dinamico no esta exigido en forma explicita… pero puede que
+encuentre que es la unica forma de resolver algunos puntos. No abuse del SQL
+dinamico, debera justificar su uso siempre.
+- Respecto a los informes XML: no se espera que produzcan un archivo nuevo en el
 filesystem, basta con que el resultado de la consulta sea XML.
-• Se espera que apliquen en todo el trabajo las pautas consignadas en la Unidad 3
-respecto a optimización de código y de tipos de datos.
+- Se espera que apliquen en todo el trabajo las pautas consignadas en la Unidad 3
+respecto a optimización de codigo y de tipos de datos.
 */
 
 EXEC msdb.dbo.sp_delete_database_backuphistory @database_name = N'Com5600G11'
@@ -111,38 +111,6 @@ BEGIN
 END
 GO
 
-DROP TABLE IF EXISTS Negocio.Expensa;
-CREATE TABLE Negocio.Expensa(
-    id INT PRIMARY KEY,
-    consorcio_id INT,
-    saldoAnterior DECIMAL(10,2),
-    ingresosEnTermino DECIMAL(10,2),
-    ingresosAdeudados DECIMAL(10,2),
-    ingresosAdelantados DECIMAL(10,2),
-    egresos DECIMAL(10,2),
-    saldoCierre DECIMAL(10,2),
-    FOREIGN KEY (consorcio_id) REFERENCES Negocio.Consorcio(id)
-);
-
-DROP TABLE IF EXISTS Negocio.DetalleExpensa;
-CREATE TABLE Negocio.DetalleExpensa(
-    id INT PRIMARY KEY,
-    expensaId INT,
-    idUnidadFuncional INT,
-    prorrateoOrdinario DECIMAL(10,2),
-    prorrateoExtraordinario DECIMAL(10,2),
-    interesMora DECIMAL(10,2),
-    totalaPagar DECIMAL(10,2),
-    saldoAnteriorAbonado DECIMAL(10,2),
-    pagosRecibidos DECIMAL(10,2),
-    primerVencimiento DATE,
-    segundoVencimiento DATE,
-    FOREIGN KEY (expensaId) REFERENCES Negocio.Expensa(id),
-    FOREIGN KEY (idUnidadFuncional) REFERENCES Consorcio.UnidadFuncional(id)
-);
-
-
-
 DROP TABLE IF EXISTS Pago.FormaDePago
 GO
 
@@ -207,12 +175,42 @@ BEGIN
 END
 GO
 
+DROP TABLE IF EXISTS Negocio.Expensa;
+CREATE TABLE Negocio.Expensa(
+    id INT PRIMARY KEY,
+    consorcio_id INT,
+    saldoAnterior DECIMAL(10,2),
+    ingresosEnTermino DECIMAL(10,2),
+    ingresosAdeudados DECIMAL(10,2),
+    ingresosAdelantados DECIMAL(10,2),
+    egresos DECIMAL(10,2),
+    saldoCierre DECIMAL(10,2),
+    FOREIGN KEY (consorcio_id) REFERENCES Negocio.Consorcio(id)
+);
+
+DROP TABLE IF EXISTS Negocio.DetalleExpensa;
+CREATE TABLE Negocio.DetalleExpensa(
+    id INT PRIMARY KEY,
+    expensaId INT,
+    idUnidadFuncional INT,
+    prorrateoOrdinario DECIMAL(10,2),
+    prorrateoExtraordinario DECIMAL(10,2),
+    interesMora DECIMAL(10,2),
+    totalaPagar DECIMAL(10,2),
+    saldoAnteriorAbonado DECIMAL(10,2),
+    pagosRecibidos DECIMAL(10,2),
+    primerVencimiento DATE,
+    segundoVencimiento DATE,
+    FOREIGN KEY (expensaId) REFERENCES Negocio.Expensa(id),
+    FOREIGN KEY (idUnidadFuncional) REFERENCES Consorcio.UnidadFuncional(id)
+);
+
+
 
 IF OBJECT_ID(N'Negocio.GastoOrdinario', 'U') IS NULL
 CREATE TABLE Negocio.GastoOrdinario (
     idGasto INT PRIMARY KEY IDENTITY,
     idExpensa INT NOT NULL,  -- FK hacia Expensa
-    idConsorcio int not null, -- fk consorcio
     nombreEmpresaoPersona VARCHAR(200) NULL,
     nroFactura VARCHAR(50) NULL,
     fechaEmision DATE NULL,
@@ -230,7 +228,6 @@ IF OBJECT_ID(N'Negocio.GastoExtraordinario', 'U') IS NULL
 CREATE TABLE Negocio.GastoExtraordinario (
     idGasto INT PRIMARY KEY IDENTITY,
     idExpensa INT NOT NULL, 
-    idConsorcio int not null, -- fk consorcio
     nombreEmpresaoPersona VARCHAR(200) NULL,
     nroFactura VARCHAR(50) NULL,
     fechaEmision DATE NULL,
@@ -246,5 +243,65 @@ ELSE
     PRINT N'Ya existe la tabla.'
 GO
 
+IF OBJECT_ID(N'Consorcio.UnidadFuncional', 'U') IS NULL
+BEGIN
+    CREATE TABLE Consorcio.UnidadFuncional
+    (
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        CVU_CBUPersona CHAR(22) NOT NULL,
+        consorcioId INT NOT NULL,
+        departamento VARCHAR(10) NULL,
+        piso VARCHAR(10) NULL,
+        numero VARCHAR(10) NULL,
+        metrosCuadrados DECIMAL(10, 2) NOT NULL,
+        porcentajeExpensas DECIMAL(5, 2) NOT NULL,
+        tipo VARCHAR(50) NULL,
+        
+        CONSTRAINT FK_UF_CuentaBancaria FOREIGN KEY (CVU_CBUPersona) 
+            REFERENCES Persona.CuentaBancaria(CVU_CBU), 
+            
+        CONSTRAINT FK_UF_Consorcio FOREIGN KEY (consorcioId) 
+            REFERENCES Consorcio.ConsorcioMaster(idConsorcio)
+    );
+END
+ELSE
+    PRINT N'Ya existe la tabla Consorcio.UnidadFuncional.';
+GO
 
---select * from Negocio.GastoExtraordinario
+---
+
+IF OBJECT_ID(N'Consorcio.Cochera', 'U') IS NULL
+BEGIN
+    CREATE TABLE Consorcio.Cochera
+    (
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        unidadFuncionalId INT NULL,
+        numero VARCHAR(10) NOT NULL,
+        porcentajeExpensas DECIMAL(5, 2) NOT NULL,
+        
+        CONSTRAINT FK_Cochera_UnidadFuncional FOREIGN KEY (unidadFuncionalId) 
+            REFERENCES Consorcio.UnidadFuncional(id)
+    );
+END
+ELSE
+    PRINT N'Ya existe la tabla Consorcio.Cochera.';
+GO
+
+---
+
+IF OBJECT_ID(N'Consorcio.Baulera', 'U') IS NULL
+BEGIN
+    CREATE TABLE Consorcio.Baulera
+    (
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        unidadFuncionalId INT NULL,
+        numero VARCHAR(10) NOT NULL,
+        porcentajeExpensas DECIMAL(5, 2) NOT NULL,
+        
+        CONSTRAINT FK_Baulera_UnidadFuncional FOREIGN KEY (unidadFuncionalId) 
+            REFERENCES Consorcio.UnidadFuncional(id)
+    );
+END
+ELSE
+    PRINT N'Ya existe la tabla Consorcio.Baulera.';
+GO
